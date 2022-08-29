e928f722859842a8d947403317dbdc0b91bb5468
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-29 10:41:10 +0100

    delete unnecessary files

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-  listOfCountries: document.querySelector('.country-list'),
-  searchInput: document.getElementById('search-box'),
-  countryCard: document.querySelector('.country-info'),
-};
